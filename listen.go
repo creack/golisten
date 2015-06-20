c00e9e106d02e42e969e68f6c175419c3e4fe1aa
Guillaume J. Charmes <guillaume@cloud.com>
2015-06-20 17:55:09 -0400

    Simplify Accept code / add standalone Listen()

--- listen.go
+++ listen.go
@@ -33,36 +33,6 @@ func lookupUser(username string) (uid, gid int, err error) {
 	return uid, gid, nil
 }
 
-// fdListener implements a listener from a file descriptor.
-type fdListener struct {
-	file *os.File
-}
-
-// Accept accepts a connection from the listener.
-// TODO: see why net.FileListener does not work. It would allow us t
-func (ln *fdListener) Accept() (net.Conn, error) {
-	fd, _, err := syscall.Accept(int(ln.file.Fd()))
-	if err != nil {
-		return nil, err
-	}
-	conn, err := net.FileConn(os.NewFile(uintptr(fd), ""))
-	if err != nil {
-		return nil, err
-	}
-	return conn.(*net.TCPConn), nil
-}
-
-// Close closes the listen fd.
-func (ln *fdListener) Close() error {
-	return ln.file.Close()
-}
-
-// Addr returns the local address of the listener.
-// TODO: implement this.
-func (ln *fdListener) Addr() net.Addr {
-	return nil
-}
-
 // ListenAndServe wraps `http.ListenAndServe`. Listen as root and accept as `targetUser`.
 func ListenAndServe(targetUser, addr string, handler http.Handler) error {
 	if !flag.Parsed() {
@@ -80,6 +50,7 @@ func ListenAndServe(targetUser, addr string, handler http.Handler) error {
 		l, err := net.Listen("tcp", addr)
 		if err != nil {
 			return fmt.Errorf("Listen error: %s", err)
+
 		}
 		f, err := l.(*net.TCPListener).File()
 		if err != nil {
@@ -109,10 +80,78 @@ func ListenAndServe(targetUser, addr string, handler http.Handler) error {
 		return nil
 	} else if u.Uid != "0" && *listenFD != 0 {
 		// We are not root and we have a listen fd. Do the accept.
-		ln := &fdListener{file: os.NewFile(uintptr(*listenFD), "net")}
-		if err := http.Serve(ln, handler); err != nil {
+		l, err := net.FileListener(os.NewFile(uintptr(*listenFD), "net"))
+		if err != nil {
+			return err
+		}
+
+		if err := http.Serve(l, handler); err != nil {
 			return err
 		}
 	}
 	return fmt.Errorf("setuid fail: %s, %d", u.Uid, *listenFD)
+}
+
+// Listen listens to the requested port and de-escalate the privilege to `targetUser`.
+func Listen(targetUser, network, addr string) (net.Listener, error) {
+	if !flag.Parsed() {
+		flag.Parse()
+	}
+	u, err := user.Current()
+	if err != nil {
+		return nil, err
+	}
+	if u.Uid != "0" && *listenFD == 0 {
+		// we are not root and we have no listen fd. Error.
+		return nil, fmt.Errorf("need to run as root. Running as %s (%s)", u.Name, u.Uid)
+	} else if u.Uid == "0" && *listenFD == 0 {
+		// we are root and we have no listen fd. Do the listen.
+		ln, err := net.Listen(network, addr)
+		if err != nil {
+			return nil, fmt.Errorf("Listen error: %s", err)
+
+		}
+		var f *os.File
+		switch ln := ln.(type) {
+		case *net.TCPListener:
+			f, err = ln.File()
+		case *net.UnixListener:
+			f, err = ln.File()
+		default:
+			err = fmt.Errorf("unsupported network: %T", ln)
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		uid, gid, err := lookupUser(targetUser)
+		if err != nil {
+			return nil, err
+		}
+		// First extra file: fd == 3
+		cmd := exec.Command(os.Args[0])
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.ExtraFiles = append(cmd.ExtraFiles, f)
+		cmd.Args = append(cmd.Args, []string{"-listen-fd", fmt.Sprint(2 + len(cmd.ExtraFiles))}...)
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			Credential: &syscall.Credential{
+				Uid: uint32(uid),
+				Gid: uint32(gid),
+			},
+		}
+		if err := cmd.Run(); err != nil {
+			return nil, fmt.Errorf("cmd.Run error: %s", err)
+		}
+		return nil, nil
+	} else if u.Uid != "0" && *listenFD != 0 {
+		// We are not root and we have a listen fd. Do the accept.
+		ln, err := net.FileListener(os.NewFile(uintptr(*listenFD), "net"))
+		if err != nil {
+			return nil, err
+		}
+		return ln, nil
+	}
+	return nil, fmt.Errorf("setuid fail: %s, %d", u.Uid, *listenFD)
 }
